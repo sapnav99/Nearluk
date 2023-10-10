82e2851e9ba9e3b9e90f035e3c73624aba0c500e
unknown <sampathd@unicsol.com>
2023-10-10 18:03:47 +0530

    added T&C and P&C pages

---
+++ src/pages/PrivacyandPolicy/PrivacyandPolicy.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+
+const PrivacyandPolicy: React.FC = () => {
+  return (
+    <div className='container'>
+        <h1>Privacy Policy for NearLuk</h1>
+        <p>Last updated: 10/10/2023</p>
+    
+        <h3>Introduction</h3>
+        <p>Welcome to NearLuk, a real estate application ("App") provided by NearLuk, Inc. 
+        ("NearLuk," "we," "us," or "our"). This Privacy Policy is designed to help you understand how we 
+        collect, use, disclose, and safeguard your personal information when you use our App and visit our
+         website at nearluk.com ("Website"). By accessing or using NearLuk, you consent to the practices 
+         described in this Privacy Policy.
+         </p>
+         <h3>Information We Collect</h3>
+         <p>1.<strong>Information You Provide:</strong> We may collect personal information you voluntarily provide, 
+            including but not limited to your name, email address, phone number, and location data when 
+            you use our App or Website.
+        </p>
+        <p>2.<strong>Usage and Device Information:</strong> We may collect information about your device and how you use the App and Website, such as your
+             IP address, browser type, device type, operating system, and usage patterns.
+        </p>
+        <p>
+            3.<strong>Location Information:</strong> With your consent, we may collect precise location information from your 
+            mobile device when you use our App to provide you with location-based services.
+        </p>
+        <h3>How We Use Your Information</h3>
+        <p><strong>We may use the information we collect for the following purposes:</strong></p>
+        <p>
+            1.To provide, maintain, and improve our App and services.
+        </p>
+        <p>
+            2.To personalize and enhance your experience on NearLuk.
+        </p>
+        <p>
+            3.To communicate with you, including sending transactional emails and notifications.
+        </p>
+        <p>
+            4.To analyze usage patterns and improve our App and services.
+        </p>
+        <p>
+            5.To comply with legal obligations and protect our rights.
+        </p>
+        <h3>Sharing Your Information</h3>
+        <p><strong>We may share your information with:</strong></p>
+        <p>1.Real estate agents and brokers to facilitate property transactions.</p>
+        <p>2.Service providers who assist us with App functionality.</p>
+        <p>3.Legal authorities to comply with applicable laws.</p>
+        <p>4.Affiliates and business partners, subject to your consent.</p>
+        <h3>Your Choices</h3>
+
+        <p>You can exercise certain rights over your personal information, including the right to access, correct,
+             or delete your data. You can also opt-out of certain communications by adjusting your account settings.
+        </p>
+        <h3>Security</h3>
+        <p>We take reasonable measures to protect your personal information. However, 
+            no method of transmission over the internet or electronic storage is
+            entirely secure. We cannot guarantee the absolute security of your data.</p>
+        <h3>Children's Privacy</h3>
+        <p>NearLuk is not intended for children under the age of 13. We do not knowingly collect or solicit personal information from children. If you
+         believe that we have collected information from a child, please contact us to have it removed.</p>
+         <h3>Changes to this Privacy Policy</h3>
+         <p>We may update this Privacy Policy to reflect changes in our practices. Please review the policy periodically for updates. 
+            The date at the top of the page indicates the last revision.</p>
+        <h3>Contact Us</h3>
+        <p>If you have any questions or concerns about this Privacy Policy, please contact us at:</p>
+        <p>
+        NearLuk, Inc. <br />
+        contact@nearluk.com
+        </p>
+    </div>
+  )
+}
+
+export default PrivacyandPolicy
