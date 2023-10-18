99c63f208cbddea24dab7fed9f4f7d6e07e7e2a7
nagasampath225 <115771025+nagasampath225@users.noreply.github.com>
2023-10-18 07:37:36 +0530

    new changes

--- src/pages/PostProperty/PostPropertyThree.tsx
+++ src/pages/PostProperty/PostPropertyThree.tsx
@@ -3,7 +3,9 @@ import { ownerShipData } from "./helper/PostPropertyData";
 import SectionHoc from "../../components/Property/SectionHoc";
 import Chip from "../../components/Chip/Chip";
 import { activateItemByKey } from "./helper/PostPropertyHelper";
-
+import PropInput from "../../components/Property/PropInput/PropInput";
+import { Checkbox } from 'antd';
+import type { CheckboxChangeEvent } from 'antd/es/checkbox';
 type Props = {};
 
 const PostPropertyThree: React.FC = (props: Props) => {
@@ -45,6 +47,61 @@ const PostPropertyThree: React.FC = (props: Props) => {
           })}
         </div>
       </SectionHoc>
+      <div className="price__details">
+        <h6>Price Details</h6>
+          <div className="price__detailing__container">
+            <PropInput placeholder="Expected Price" value={stepThreeData.expected_price} onChange={(e: any) => {
+              setStepThreeData({
+                ...stepThreeData,
+                expected_price: e.target.value
+              })
+            } } />
+            <PropInput placeholder="Mainatinace per Month" value={stepThreeData.maintainance_per_month} onChange={(e: any) => {
+              setStepThreeData({
+                ...stepThreeData,
+                maintainance_per_month: e.target.value
+              })
+            } } />
+            <PropInput placeholder="Token Amount" value={stepThreeData.token_amount} onChange={(e: any) => {
+              setStepThreeData({
+                ...stepThreeData,
+                token_amount: e.target.value
+              })
+            } } />
+          </div>
+          <div className="price__details__checkbox_container">
+            <Checkbox onChange={(e: CheckboxChangeEvent) => {
+              setStepThreeData({
+                ...stepThreeData,
+                negotiable: e.target.checked
+              })
+            }} >Price Negotiable</Checkbox>
+            <Checkbox onChange={(e: CheckboxChangeEvent) => {
+              setStepThreeData({
+                ...stepThreeData,
+                loan: e.target.checked
+              })
+            }} >Currently Under Loan</Checkbox>
+            <Checkbox onChange={(e: CheckboxChangeEvent) => {
+              setStepThreeData({
+                ...stepThreeData,
+                all_inclusive_price: e.target.checked
+              })
+            }} >All Inclusive Price</Checkbox>
+            <Checkbox onChange={(e: CheckboxChangeEvent) => {
+              setStepThreeData({
+                ...stepThreeData,
+                annual_dues_pay: e.target.checked
+              })
+            }} >Annual Dues Paid</Checkbox>
+            <Checkbox onChange={(e: CheckboxChangeEvent) => {
+              setStepThreeData({
+                ...stepThreeData,
+                tax_excluded: e.target.checked
+              })
+            }} >Tax Excluded</Checkbox>
+          </div>
+      </div>
     </div>
   );
 };
